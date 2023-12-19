198f23dda6ea8cc4567db0b6628730db2c4d4a63
20yuteo <20yuteo@gmail.com>
2023-12-19 20:10:21 +0900

    first commit

---
+++ main/entry.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	ginadapter "github.com/awslabs/aws-lambda-go-api-proxy/gin"
+	"github.com/gin-gonic/gin"
+)
+
+var ginLambda *ginadapter.GinLambda
+
+func init() {
+	log.Printf("Gin cold start")
+
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	ginLambda = ginadapter.New(r)
+}
+
+func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return ginLambda.ProxyWithContext(ctx, req)
+}
+
+func main() {
+	lambda.Start(Handler)
+}
